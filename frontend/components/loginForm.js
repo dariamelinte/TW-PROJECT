1b0e82e367837ae3f483c2bd1a332008e2e18163
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2023-04-03 16:22:55 +0300

    added methods in component body

--- frontend/components/loginForm.js
+++ frontend/components/loginForm.js
@@ -1,6 +1,19 @@
 export default function LoginForm() {
   const form = document.createElement('form');
 
+  const login = ((event) => {
+    event.preventDefault();
+
+    
+    const form = document.querySelector('form');
+    const formData = new FormData(form);
+    
+    //TODO: send the data to the server
+    console.log(formData.get('email'));
+    console.log(formData.get('password'));
+  ;;}).toString().replace('(event) => {', '').replace(';;}', '');
+
+
   form.innerHTML = `
     <div class="flex column justify-center">
       <label for="email">Email</label>
@@ -15,7 +28,7 @@ export default function LoginForm() {
     <a class="mt-2" href="/register">Add your family</a>
     <a href="/forgot-password">Forgot password?</a>
 
-    <button class="mt-3">Login</button>
+    <button class="mt-3" onclick="${login}">Login</button>
     `;
   return form;
 }
