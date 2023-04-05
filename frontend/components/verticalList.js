c3291992d8028991b6287c0a8b6271854cf86e06
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2023-04-06 01:52:14 +0300

    created all app branches

--- frontend/components/verticalList.js
+++ frontend/components/verticalList.js
@@ -2,14 +2,14 @@ export default function VerticalList(items) {
   const childId = new URLSearchParams(window.location.search).get('childId');
   const list = document.createElement('div');
 
-  list.className = 'flex column justify-center align-center mt-9';
+  list.className = 'flex column justify-center align-center mt-9 w-20';
 
   items.forEach(item => {
     const listItem = document.createElement('div');
-    listItem.className = 'bg-purple-500 rounded-lg p-3 mt-1 rounded-lg cursor-pointer';
+    listItem.className = 'w-full center bg-purple-500 rounded-lg p-3 mt-2 rounded-lg cursor-pointer';
     listItem.onclick = () => window.location.href = item.path(childId);
     listItem.innerHTML = `
-      <h2 class="text-white light-bold-text ">${item.title}</h2>
+      <h2 class="text-purple-900 bold-text ">${item.title}</h2>
     `;
     list.appendChild(listItem);
   });
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,38 @@
 import Header from '../components/header.js';
 import VerticalList from '../components/verticalList.js';
 import Routes from '../utils/Routes.js';
+import ChildrenList from '../components/children/childrenList.js';
 
 document.body.appendChild(Header({ iconPath: '../assets' }));
-document.body.appendChild(VerticalList(Object.values(Routes.children)));
+
+// if the childId param is present, then the user is on a child page
+const childId = new URLSearchParams(window.location.search).get('childId');
+
+const mock = [
+  {
+    "id": 1,
+    "name": "Eva Maria"
+  },
+  {
+    "id": 2,
+    "name": "Marco"
+  },
+  {
+    "id": 3,
+    "name": "Felix"
+  },
+  {
+    "id": 4,
+    "name": "Lulu"
+  },
+  {
+    "id": 5,
+    "name": "Roger"
+  }
+]
+
+if (childId) {
+  document.body.appendChild(ChildrenList(mock));
+} else {
+  document.body.appendChild(VerticalList(Object.values(Routes.children)));
+}
