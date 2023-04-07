bcd7d4bd5bf96425d120eac1b73d81deebdfa9f0
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2023-04-07 19:23:47 +0300

    removed components

--- frontend/pages/child/medical-history/card/medicalHistoryCard.js
+++
@@ -1,5 +0,0 @@
-import Header from "../../../../components/header.js";
-import MedicalHistoryForm from "../../../../components/forms/medicalHistoryForm.js";
-
-document.body.appendChild(Header("Istoric medical"));
-document.body.appendChild(MedicalHistoryForm());
