6df499994c1ec1805afd8a688e23bc2cee847cfa
daria melinte <dariamelinte2003@gmail.com>
2023-06-17 23:16:44 +0300

    fix jwt parse

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,7 +6,7 @@ import { getFamilyChildren } from "../server/child/getFamilyChildren.js";
 
 import { getCookie } from '../utils/cookie.js';
 import { COOKIE_NAME } from '../utils/constants.js';
-import { isJwtExpired } from '../utils/jwt.js';
+import { isJwtExpired, parseJwt } from '../utils/jwt.js';
 import Routes from '../utils/Routes.js';
 
 const jwt = getCookie(COOKIE_NAME);
@@ -14,6 +14,8 @@ const jwt = getCookie(COOKIE_NAME);
 if (!jwt || isJwtExpired(jwt)) {
   window.location.href = Routes.login.path();
 }
+
+console.log(parseJwt(jwt));
 
 // if the childId param is present, then the user is on a child page
 const childId = new URLSearchParams(window.location.search).get('childId');
--- frontend/server/child/getFamilyChildren.js
+++ frontend/server/child/getFamilyChildren.js
@@ -1,9 +1,10 @@
 import { COOKIE_NAME, SERVER_URL } from '/frontend/utils/constants.js';
+import { getCookie } from '/frontend/utils/cookie.js';
 import { parseJwt } from '/frontend/utils/jwt.js';
 
 export const getFamilyChildren = async () => {
   try {
-    const userInfo = parseJwt(COOKIE_NAME);
+    const userInfo = parseJwt(getCookie(COOKIE_NAME));
     // TODO: send jwt to validate permission
     const data = await fetch(`${SERVER_URL}/child?familyId=${userInfo.familyId}`);
     const { success, result } = await data.json() || {};
