6840f6c86a578e3c5fa04cb2e516a4cf13826558
daria melinte <dariamelinte2003@gmail.com>
2023-06-19 08:36:39 +0300

    revert user route ids

--- server/src/routers/user.js
+++ server/src/routers/user.js
@@ -14,7 +14,7 @@ const {
 
 async function userRouter(res, pool) {
   try {
-    const { url, method, body, id: userId, familyId: userFamilyId } = res.locals;
+    const { url, method, body } = res.locals;
     const { id, email, familyId } = querystring.parse(getQueryString(url));
 
     let resStatusCode = StatusCodes.NOT_FOUND;
@@ -24,17 +24,17 @@ async function userRouter(res, pool) {
     };
 
     if (routes.user.getByEmail.validate(url, method)) {
-      const { statusCode, data } = await getUserByEmail(pool, email, userId);
+      const { statusCode, data } = await getUserByEmail(pool, email);
 
       resStatusCode = statusCode;
       resData = data;
     } else if (routes.user.getById.validate(url, method, id)) {
-      const { statusCode, data } = await getUserById(pool, id, userId);
+      const { statusCode, data } = await getUserById(pool, id);
 
       resStatusCode = statusCode;
       resData = data;
     } else if (routes.user.getByFamilyId.validate(url, method, familyId)) {
-      const { statusCode, data } = await getUsersByFamilyId(pool, familyId, userFamilyId);
+      const { statusCode, data } = await getUsersByFamilyId(pool, familyId);
 
       resStatusCode = statusCode;
       resData = data;
@@ -44,12 +44,12 @@ async function userRouter(res, pool) {
       resStatusCode = statusCode;
       resData = data;
     } else if (routes.user.update.validate(url, method, id)) {
-      const { statusCode, data } = await updateUser(pool, id, body, userId);
+      const { statusCode, data } = await updateUser(pool, id, body);
 
       resStatusCode = statusCode;
       resData = data;
     } else if (routes.user.delete.validate(url, method, id)) {
-      const { statusCode, data } = await deleteUser(pool, id, userId);
+      const { statusCode, data } = await deleteUser(pool, id);
 
       resStatusCode = statusCode;
       resData = data;
